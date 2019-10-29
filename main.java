e212c24642943544b6067cffbb1409490e57a97b
eileenyoo2 <48309864+eileenyoo2@users.noreply.github.com>
2019-10-29 17:52:04 +0900

    main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+		main2 m2 = new main2();
+		MultiChatUI2 ui = new MultiChatUI2();
+		
+		m2.start();
+		//mu.start();
+
+	}
+
+}
